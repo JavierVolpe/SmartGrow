c1cce8e93ea13748fdfa53ba6c9e3c5c64f0fb65
JavierVolpe <javo0001@stud.kea.dk>
2024-11-17 21:49:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask_login import LoginManager
 import secrets
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
 
 # Initialize Flask app
 app = Flask(__name__)
 #app.secret_key = secrets.token_hex(16)  # Or use your existing secret key
 app.secret_key = "AAAAAAAAAAAAAAAA"
 
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+
+# Configure the Rotating File Handler
+handler = RotatingFileHandler('logs/log_app.log', maxBytes=100000, backupCount=10)
+formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+handler.setFormatter(formatter)
+handler.setLevel(logging.INFO)
 
 
 # Initialize Flask-Login
@@ -14,8 +26,13 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
+
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
+app.logger.info('Application startup')
+
 # Import routes (make sure this import comes after initializing app and login_manager)
 from routes import *
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
