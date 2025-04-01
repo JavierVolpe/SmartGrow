73ea3bb370e271eda8bcc340eb40a6a2849c6dfa
Javier Alejandro Volpe <javo0001@stud.kea.dk>
2025-04-01 19:50:39 +0200

    Update run_hidden.sh

--- run_hidden.sh
+++ run_hidden.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /home/javier/SmartHome || exit
-source SmartGrow/bin/activate
-nohup python app.py > log_app.log 2>&1 &
+cd /home/debian/SmartGrow || exit
+source venv/bin/activate
 
+nohup python app.py > log_app.log 2>&1 &
 nohup python log_data.py > log_data.log 2>&1 &
